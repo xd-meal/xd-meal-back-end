31e57dd3fe420952256208fe5087df72950485b9
shenzh <shenzhuo@xindong.com>
2019-12-25 10:43:42 +0800

    【fix】httpGet add log time param

--- Function/curl.go
+++ Function/curl.go
@@ -9,7 +9,7 @@ import (
 )
 
 func HttpGet(url string) (string, error) {
-	timeStart := time.Now().Unix()
+	timeStart := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
 		logging.Error("httpGet error1")
@@ -22,7 +22,7 @@ func HttpGet(url string) (string, error) {
 		logging.Error("httpGet error2")
 		return "", err
 	}
-	timeEnd := time.Now().Unix()
-	logging.Info(fmt.Sprintf("httpGet : (url: %s code: %s, response body: %s, elaspsed time:%d s)", url, resp.Status, string(body), timeEnd-timeStart))
+	timeEnd := time.Now()
+	logging.Info(fmt.Sprintf("httpGet : (url: %s code: %s, response body: %s, elaspsed time:%d s)", url, resp.Status, string(body), timeEnd.Sub(timeStart)))
 	return string(body), nil
 }
